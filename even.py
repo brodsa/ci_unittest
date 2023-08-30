f5daa50355f4b9d61ed4628c3cf68b856bd5df4c
brodsa <brodinova@gmail.com>
2023-08-30 15:20:25 +0000

    test: write test for even_number_of_evens

--- even.py
+++ even.py
@@ -7,11 +7,20 @@ def even_number_of_evens(numbers):
     if the numner of even numbers is even - return True
     """
     if isinstance(numbers, list):
-        return True
+        if numbers == []:
+            return False
+        else:
+            evens = 0
+            
+        for n in numbers:
+            if n % 2 == 0:
+                evens += 1
+        if evens:
+            return evens % 2 == 0
+        else:
+            return False
     else:
         raise TypeError("A list was not passed into the function")
-
-
     return None
 
 if __name__ == "__main__":
