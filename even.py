79aa3c6c8470b5e5d0644b41c650ab0f7443d905
brodsa <brodinova@gmail.com>
2023-08-30 15:04:52 +0000

    test: write fail test for raising TypeError

--- even.py
+++ even.py
@@ -6,6 +6,11 @@ def even_number_of_evens(numbers):
     if the number of even numbers is odd - return False
     if the numner of even numbers is even - return True
     """
+    if isinstance(numbers, list):
+        return True
+    else:
+        raise TypeError("A list was not passed into the function")
+
 
     return None
 
